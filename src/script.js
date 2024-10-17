18111ec00ea7a29478a5bea9bf818d3f1d2019ef
adriantayeh <adriantayeh@gmail.com>
2024-10-17 09:30:55 +0200

    added blur on menu hover

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const headerLink = document.querySelectorAll("li.navLink");
 const dropdown = document.querySelectorAll("div.dropdownNav");
+const mainContent = document.querySelector("main");
 
 headerLink.forEach((item, index) => {
   item.addEventListener("mouseenter", () => {
@@ -15,6 +16,12 @@ headerLink.forEach((item, index) => {
       dropdown[index + 1].classList.remove("flex");
       dropdown[index + 1].classList.add("hidden");
     }
+    if (index !== 0) {
+      mainContent.classList.add("blur");
+    }
+    if(index == 0) {
+      mainContent.classList.remove("blur");
+    }
   });
 });
 
@@ -22,9 +29,16 @@ dropdown.forEach((item, index) => {
   item.addEventListener("mouseenter", () => {
     dropdown[index].classList.remove("hidden");
     dropdown[index].classList.add("flex");
+    if (index !== 0) {
+      mainContent.classList.add("blur");
+    }
+    else {
+      mainContent.classList.remove("blur");
+    }
   });
   item.addEventListener("mouseleave", () => {
     dropdown[index].classList.remove("flex");
     dropdown[index].classList.add("hidden");
+    mainContent.classList.remove("blur");
   });
 });
